65317606108121e0aa45d1c5444a0acc46a51a8a
zhouyida <zhouyizhe0926@gmail.com>
2019-08-30 23:26:45 +0800

    change commit

--- java/src/main/java/Test/Adrealtime.java
+++
@@ -1,9 +0,0 @@
-package Test;
-
-import java.sql.SQLOutput;
-
-public class Adrealtime {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
--- java/src/main/java/Test/test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-read name
-echo "$name It is a test"
-users="file1"
-$ who > users
-$ echo "菜鸟教程：www.runoob.com" >> users
